969406c450c9798c3dd3c866d51df94b7fdeec8d
Noor <noormir1295@gmail.com>
2020-08-25 20:07:44 -0400

    update javascript

--- index.js
+++ index.js
@@ -1,142 +1,170 @@
-const fs=require("fs");
+const fs = require("fs");
 
-const inquirer=require("inquirer");
+const inquirer = require("inquirer");
 
-const questions=[
-    //ask the user for the name of the app
-    {
-        type="input",
-        name="repoName",
-        massage="What is the name of this application?"
-    },
-    //ask for the users github username
-    {
-        type="input",
-        name="githubName",
-        message="What is your github username?"
-    },
-    //ask for the users description of the project
-    {
-        type="input",
-        name="description",
-        message="Write a discription about this application"
-    },
-    // How do you install your app? 
-    {
-        type: "input",
-        message: "How does a user install your application?",
-        name: "installApp",
-     
-    },
-    //How does user use their app
-    {
-        type:"input",
-        name:"usage",
-        message:"Provide instructions on how a user will use your applications"
-    },
-    //How would a user of the application report an issue
-    {
-        type:"input",
-        name:"issues",
-        message:"Will users be able to report issues for testing purposes?"
-    },
-    //Type of license being used(badge)
-    {
-        type:"input",
-        name:"license",
-        message:"What type of license will you be using? If you do not have a license, please type leave empty."
-    },
-    //Who else will contribute to the application other than the user
-    {
-        type:"input",
-        name:"contributors",
-        message:"Who else will be contributing in this appliation?"
-    }
-
+const questions = [
+  //ask the user for the name of the app
+  {
+    type: "input",
+    name: "repoName",
+    message:
+      "What is name of this application? (This will aslo the main header of the README)",
+  },
+  //ask for the users github username
+  {
+    type: "input",
+    name: "githubName",
+    message: "What is your username on Github?",
+  },
+  //ask for the users description of the project
+  {
+    type: "input",
+    message: "Write a description of this application",
+    name: "description",
+  },
+  // How do you install your app?
+  {
+    type: "input",
+    message:
+      "How does a user install your application? Provide instuctions here",
+    name: "appInstall",
+  },
+  // How does a user use your application?
+  {
+    type: "input",
+    message: "How does a user use your application?",
+    name: "appInstructions",
+  },
+  //How would a user of the application report an issue
+  {
+    type: "input",
+    message:
+      "Would you like you users to be able to report issues or contribute to this project?",
+    name: "appIssues",
+  },
+  //Type of license being used
+  {
+    type: "input",
+    message:
+      'What license they would you  like to use? If you do not wish to use a license type "none" ',
+    name: "license",
+  },
+  //Who else will contribute to the application other than the user
+  {
+    type: "input",
+    message: "Who else contributed to your application?",
+    name: "contributors",
+  },
 ];
 
-function init() {
-inquirer.prompt(questions).then((response)=>{
-//creating the first line and main header for the repo using "#" as a header markdown
-
-fs.appendFileSync("README.md",("#") + response.repoName + '\n', function(err){
-    if(err) {
-        console.log(err)
-    }else {
-        console.log("Successfull")
-    }
-});
-
-//create second line that will format the users github name and who is developing the project
-fs.appendFileSync("README.md",("This application was developed by: " + response.githubName + '\n') + '\n', function(err){
-    if(err){
-        console.log(err)
-    }else{
-        console.log("SuccessfUll")
-    }
-});
-
-//user can add description of the project
-fs.appendFileSync("README.md",("## Description" + '\n' + response.description) + '\n', function(err){
-    if (err) {
-        console.log(err)
-    }else{
-        console.log("SuccessfUll")
-    }
-});
-
-//creating section for user to describe how to install the app
-fs.appendFileSync("README.md",("## Application Installation" + '\n' + response.installApp) + '\n', function(err) {
-     if (err) {
-        console.log(err)
-    }else{
-        console.log("SuccessfUll")
-    }
-});
-
-//creating section for users to describe the usage of Application 
-fs.appendFileSync("README.ms",("##Application Usage" + '\n' + response.usage) + '\n', function(err){
-if (err) {
-        console.log(err)
-    }else{
-        console.log("SuccessfUll")
-    }
-});
-
-//section for user to describe issues
-fs.appendFileSync("README.md",("##ISSUES" + '\n' + response,issues) + '\n', function(err){
-    if (err) {
-        console.log(err)
-    }else{
-        console.log("SuccessfUll")
-    }
-});
-
-//section for user to input other developers that are contributing 
-fs.appendFileSync("README.md", ("## Other Contributors" + '\n' + response.contributors) + '\n', function(err) {
-    if (err) {
-        console.log(err)
-    }else{
-        console.log("SuccessfUll")
-    }
-});
-
-//section for licenses being used by user
-fs.appendFileSync("README.md", ("## License" + '\n' + response.license) + '\n', function(err){
-     if (err) {
-        console.log(err)
-    }else{
-        console.log("SuccessfUll")
-    }
-});
-
-
-
-
-
-});
-
+async function init() {
+  const response = await inquirer.prompt(questions);
+  const handleResponse = (response) => {
+    //creating first header for title of the project the user is doing
+    fs.appendFileSync(
+      "README.md",
+      "# " + response.repoName + "\n" + "\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Success");
+        }
+      }
+    );
+    // section that will create the github username of the user
+    fs.appendFileSync(
+      "README.md",
+      "This application was developed by: " + response.githubName + "\n" + "\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Success");
+        }
+      }
+    );
+    // section that will create the description of what the project is about
+    fs.appendFileSync(
+      "README.md",
+      "## Description" + "\n" + response.description + "\n" + "\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Success");
+        }
+      }
+    );
+    //creates a section for what installation will be used for the users app
+    fs.appendFileSync(
+      "README.md",
+      "## Installation" + "\n" + response.appInstall + "\n" + "\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Success");
+        }
+      }
+    );
+    // section where the user will discuess how others will use the application
+    fs.appendFileSync(
+      "README.md",
+      "## How to use the Application" +
+        "\n" +
+        response.appInstructions + "\n" + "\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Success");
+        }
+      }
+    );
+    // section that asks users if they will allow users to report issues
+    fs.appendFileSync(
+      "README.md",
+      "## Issue Reporting and Contributing" +
+        "\n" +
+        response.appIssues + "\n" +"\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Success");
+        }
+      }
+    );
+    // section where users can add other contributors to project
+    fs.appendFileSync(
+      "README.md",
+      "## Other Contibuting Developers:" +
+        "\n" +
+        response.contributors + "\n" + "\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Success");
+        }
+      }
+    );
+    // section for user to show licneses being used
+    fs.appendFileSync(
+      "README.md",
+      "## License" + "\n" + response.license + "\n" + "\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Success");
+        }
+      }
+    );
+  };
+  handleResponse(response);
 }
 
-//run the function of questions in order to generate Readme file
+//command to run the README.md file
 init();
